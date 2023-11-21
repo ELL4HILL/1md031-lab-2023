8e16c7f87d79db003ed040cb42ea68c220f35c98
ELL4HILL <ell4.hill@gmail.com>
2023-11-21 11:34:30 +0100

    add middle of JS

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,8 +1,17 @@
 <template>
-    <div>
-      {{ burger.name }} {{ burger.kCal }}
-    </div>
-  </template>
+  <div class="box">
+    <h3> {{ burger.name }}</h3>
+    <img :src="burger.imageURL" width="200" height="250">
+    <ul>
+      <li> {{burger.kcal}} kalorier </li>
+      <li v-if="burger.lactose">Innehåller laktos</li>
+      <li v-if="burger.gluten" >Innehåller <span class="glutenklass"> gluten </span> </li>
+      <!-- Andra detaljer om burgaren beroende på dess egenskaper -->
+    </ul>
+  </div>
+</template>
+
+  //  
   
   <script>
   export default {
@@ -15,6 +24,19 @@
   
   <!-- Add "scoped" attribute to limit CSS to this component only -->
   <style scoped>
-  
+
+.box {
+  background-color: #f0e3e9;
+  color: rgb(0, 0, 0);
+  border-radius: 50px;
+  padding: 20px;
+  width: 12em;
+}
+
+ .glutenklass {
+  font-weight: bolder;
+ }
   </style>
+  
+
   
